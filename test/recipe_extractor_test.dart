c49ff4fd7f55d95fca922d8e742bdb84bc13ab72
JdM <julien@rmbi.ch>
2024-04-23 19:11:52 +0200

    setup workflow

--- test/recipe_extractor_test.dart
+++ test/recipe_extractor_test.dart
@@ -1,12 +1,6 @@
-import 'package:recipe_extractor/recipe_extractor.dart';
 import 'package:test/test.dart';
 
 void main() {
-  extractRecipe(
-          "https://www.topsecretrecipes.com/jack-links-original-beef-jerky-copycat-recipe.html")
-      .then((value) {
-    print(value.toMap());
-  });
   group('A group of tests', () {
     // final awesome = Awesome();
 
